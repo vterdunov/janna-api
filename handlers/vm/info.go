7ff6ab877a053cebf39a1fb11abf674a3565c60d
vterdunov <mail2slick@gmail.com>
2018-01-08 19:03:38 +0300

    Get info about VM works now

--- handlers/vm/info.go
+++ handlers/vm/info.go
@@ -2,23 +2,47 @@ package vm
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 
+	"github.com/rs/zerolog/log"
+	"github.com/vmware/govmomi/vim25/types"
 	vmware "github.com/vterdunov/janna-api/providers/vmware/vm"
 )
 
+type info struct {
+	Guest     *types.GuestInfo                  `json:"Guest,omitempty"`
+	Heartbeat types.ManagedEntityStatus         `json:"HeartBeat,omitempty"`
+	Runtime   types.VirtualMachineRuntimeInfo   `json:"Runtime,omitempty"`
+	Config    types.VirtualMachineConfigSummary `json:"Config,omitempty"`
+}
+
+// ReadInfo get information about VMs
 func ReadInfo(w http.ResponseWriter, r *http.Request) {
 	vmName := r.FormValue("vmname")
-	information, _ := vmware.VMInfo(vmName)
-
-	body, err := json.Marshal(information)
+	inf, err := vmware.VMInfo(vmName)
 	if err != nil {
-		log.Printf("Couldn't encode info data: %v", err)
+		log.Error().Err(err).Msg("Couldn't get info about VM")
 		http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
 		return
 	}
-	// fmt.Fprint(w, information)
+
+	resp := &info{}
+	for _, vmInfo := range inf {
+		resp.Guest = vmInfo.Guest
+		resp.Heartbeat = vmInfo.GuestHeartbeatStatus
+		resp.Runtime = vmInfo.Summary.Runtime
+		resp.Config = vmInfo.Summary.Config
+	}
+
+	body, err := json.Marshal(resp)
+	if err != nil {
+		log.Error().
+			Err(err).
+			Str("vmname", vmName).
+			Msg("Couldn't encode info data")
+		http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(body)
--- providers/vmware/vm/info.go
+++ providers/vmware/vm/info.go
@@ -2,9 +2,9 @@ package vmware
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"os"
+
+	"github.com/rs/zerolog/log"
 
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/find"
@@ -14,107 +14,63 @@ import (
 	"github.com/vmware/govmomi/vim25/types"
 )
 
-type VMInfoData struct {
-	ipAddress string
-	name      string
-}
-
 // VMInfo returns slice of Virtual Machines
-func VMInfo(vmName string) (*VMInfoData, error) {
-	url := os.Getenv("VMWARE_URI")
-	u, _ := soap.ParseURL(url)
+func VMInfo(vmName string) ([]mo.VirtualMachine, error) {
+	vmWareURI := os.Getenv("VMWARE_URI")
+	u, _ := soap.ParseURL(vmWareURI)
 
+	// TODO: Get from config
 	insecure := true
+
 	ctx := context.Background()
 
 	c, err := govmomi.NewClient(ctx, u, insecure)
 	if err != nil {
-		log.Print(err)
+		log.Error().Err(err).Msg("Cannot create govmomi client")
 	}
 
 	defer c.Logout(ctx)
 	f := find.NewFinder(c.Client, true)
 
 	dc, err := f.DatacenterOrDefault(ctx, "/DC1")
-
 	if err != nil {
-		log.Print(err)
+		log.Error().Err(err).Msg("Cannot found the Datacenter")
 	}
 
 	f.SetDatacenter(dc)
 
-	vms, err := f.VirtualMachineList(ctx, vmName)
+	vmObjs, err := f.VirtualMachineList(ctx, vmName)
 	if err != nil {
 		if _, ok := err.(*find.NotFoundError); ok {
-			log.Println("Not found errorr.")
+			log.Error().Err(err).Msg("Cannot found VM")
 			return nil, err
 		}
-		log.Print(err)
+		log.Error().Err(err)
 		return nil, err
 	}
 
-	pc := property.DefaultCollector(c.Client)
-
-	// Retrieve summary property
-	// Reference: http://pubs.vmware.com/vsphere-60/topic/com.vmware.wssdk.apiref.doc/vim.VirtualMachine.html
-	props := []string{
-		"summary",
-		"guest.ipAddress",
-		"config.extraConfig",
-		"datastore",
-		"network",
-		"config.tools",
-	}
-
-	// Convert datastores into list of references
-	var refs []types.ManagedObjectReference
-	for _, vm := range vms {
+	refs := make([]types.ManagedObjectReference, 0, len(vmObjs))
+	for _, vm := range vmObjs {
 		refs = append(refs, vm.Reference())
 	}
 
-	// Retrieve name property for all vms
-	var vmt []mo.VirtualMachine
-	err = pc.Retrieve(ctx, refs, props, &vmt)
-	if err != nil {
-		log.Fatal(err)
+	var vms []mo.VirtualMachine
+	// Retrieve all properties
+	// Reference: http://pubs.vmware.com/vsphere-60/topic/com.vmware.wssdk.apiref.doc/vim.VirtualMachine.html
+	var props []string
+	props = nil
+
+	pc := property.DefaultCollector(c.Client)
+
+	if len(refs) != 0 {
+		err = pc.Retrieve(ctx, refs, props, &vms)
+		if err != nil {
+			log.Error().Msg("Cannot retreive inforamtion about VM")
+			return nil, err
+		}
 	}
 
-	vmInf := &VMInfoData{}
-	// Print name per virtual machine
-	fmt.Println("Virtual machines found:", len(vmt))
-	for _, vm := range vmt {
-		fmt.Println(vm.Guest.ToolsVersion)
-		vmInf.ipAddress = vm.Guest.IpAddress
-		vmInf.name = vm.Name
-	}
+	log.Info().Int("count", len(vms)).Msg("Virtual machines found")
 
-	return vmInf, nil
-	// // Create view of VirtualMachine objects
-	// m := view.NewManager(c.Client)
-
-	// v, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"VirtualMachine"}, true)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// defer v.Destroy(ctx)
-
-	// // Retrieve summary property for all machines
-	// // Reference: http://pubs.vmware.com/vsphere-60/topic/com.vmware.wssdk.apiref.doc/vim.VirtualMachine.html
-	// var vms []mo.VirtualMachine
-	// refs := []string{"VirtualMachine"}
-	// props := []string{
-	// 	"summary",
-	// 	"guest.ipAddress",
-	// 	"config.extraConfig",
-	// 	"datastore",
-	// 	"network",
-	// 	"config.tools",
-	// }
-
-	// err = v.Retrieve(ctx, refs, props, &vms)
-	// if err != nil {
-	// 	log.Fatal(err) // TODO: log the error and return 500
-	// }
-
+	return vms, nil
 }
