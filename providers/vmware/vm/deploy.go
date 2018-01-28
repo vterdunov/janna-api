c7fcb7bd1dbb209e600fe5f520734c1fb6be4b7a
vterdunov <mail2slick@gmail.com>
2018-01-28 20:32:23 +0300

    add todo to deploy method

--- providers/vmware/vm/deploy.go
+++ providers/vmware/vm/deploy.go
@@ -9,34 +9,29 @@ import (
 
 // Deploy returns summary information about Virtual Machines
 func Deploy(ctx context.Context, vmName string, OVAURL string, logger log.Logger, cfg *config.Config, opts ...string) (int, error) {
+	// TODO: make up a metod to check deploy progress.
+	// Job ID and endpoint with status?
+	// keep HTTP connection with client and poll it?
 	var jid int
-	// vmWareURL := cfg.Vmware.URL
 
-	// u, err := soap.ParseURL(vmWareURL)
-	// if err != nil {
-	// 	logger.Log("err", "cannot parse VMWare URL")
-	// 	return jid, err
-	// }
+	// 1) create empty struct that represents a deploy object
 
-	// insecure := cfg.Vmware.Insecure
+	// 2) Run a chain of calls:
+	// 3) "Prepare":
+	// 	 - validate OVA URL (try to use vim25.Client as Opener it has OpemRemote method)
+	//   - fil OVA struct with: vim25.Client, Datacenter, Datastore, ResourcePool
+	// 4) Download OVA OR use vim25.Client as Opener (see govc importx/archive.go:143)
+	// 5) Import OVA (see govc importx)/ovf.go:212) it returns *types.ManagedObjectReference
+	//   - Read file to []byte and unmarshal it to ovf.Envelope. The type allows to access to fileds of OVF strucure
+	//   - Create a struct that represents a pert of OVF spec: name, nwtwork, etc.
+	// 6) Create OVF Manager (ovf.NewManager)
+	// 7) Fil/overrride the spec into current OVF (ovf.CreateImportSpec)
+	// ?
+	// 9) Get VM Folder.
+	// 10) Crate Lease object (optional)
+	// 11) Upload OVF
+	// 12) ... Start
 
-	// c, err := govmomi.NewClient(ctx, u, insecure)
-	// if err != nil {
-	// 	logger.Log("err", err)
-	// 	return jid, err
-	// }
-
-	// defer c.Logout(ctx)
-	// f := find.NewFinder(c.Client, true)
-
-	// dcName := cfg.Vmware.DC
-	// dc, err := f.DatacenterOrDefault(ctx, dcName)
-	// if err != nil {
-	// 	logger.Log("err", err)
-	// 	return jid, err
-	// }
-
-	// f.SetDatacenter(dc)
 	logger.Log(
 		"msg", "Deploy OVA",
 		"name", vmName,
