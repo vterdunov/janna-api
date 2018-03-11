79483a8fd18d1281c8a27f8d7c45d5a375502a8a
vterdunov <mail2slick@gmail.com>
2018-03-12 00:21:11 +0300

    Move part of code to Import method

--- providers/vmware/vm/deploy.go
+++ providers/vmware/vm/deploy.go
@@ -146,6 +146,196 @@ func (o *deployment) Upload(ctx context.Context, lease *nfc.Lease, item nfc.File
 	return lease.Upload(ctx, item, f, opts)
 }
 
+func (o *deployment) Import(ctx context.Context, pathToOVF string) (*types.ManagedObjectReference, error) {
+
+	rovf, err := readOVF(pathToOVF)
+	if err != nil {
+		return nil, errors.Wrap(err, "Could not read OVF file")
+	}
+
+	e, err := readEnvelope(rovf)
+	if err != nil {
+		return nil, errors.Wrap(err, "Could not read Envelope")
+	}
+
+	name := "Virtual Appliance"
+	if e.VirtualSystem != nil {
+		name = e.VirtualSystem.ID
+		if e.VirtualSystem.Name != nil {
+			name = *e.VirtualSystem.Name
+		}
+	}
+
+	// Override name from params if specified
+	if o.Name != "" {
+		name = o.Name
+	}
+
+	cisp := types.OvfCreateImportSpecParams{
+		// See https://github.com/vmware/govmomi/blob/v0.16.0/vim25/types/enum.go#L3381-L3395
+		// VMWare can not support some of those disk format types
+		// "preallocated", "thin", "seSparse", "rdm", "rdmp",
+		// "raw", "delta", "sparse2Gb", "thick2Gb", "eagerZeroedThick",
+		// "sparseMonolithic", "flatMonolithic", "thick"
+		DiskProvisioning: "thin",
+		EntityName:       name,
+		NetworkMapping:   o.NetworkMap(e),
+	}
+
+	m := ovf.NewManager(o.Client)
+	rp := o.ResourcePool
+	ds := o.Datastore
+	spec, err := m.CreateImportSpec(ctx, string(rovf), rp, ds, cisp)
+	if err != nil {
+		return nil, errors.Wrap(err, "Could not create VM spec")
+	}
+	if spec.Error != nil {
+		return nil, errors.New(spec.Error[0].LocalizedMessage)
+	}
+
+	// TODO: get from params
+	anno := "Test annotations"
+	if anno != "" {
+		switch s := spec.ImportSpec.(type) {
+		case *types.VirtualMachineImportSpec:
+			s.ConfigSpec.Annotation = anno
+		case *types.VirtualAppImportSpec:
+			s.VAppConfigSpec.Annotation = anno
+		}
+	}
+
+	lease, err := rp.ImportVApp(ctx, spec.ImportSpec, o.Folder, o.Host)
+	if err != nil {
+		return nil, errors.Wrap(err, "Could not import Virtual Appliance")
+	}
+
+	info, err := lease.Wait(ctx, spec.FileItem)
+	if err != nil {
+		return nil, err
+	}
+
+	u := lease.StartUpdater(ctx, info)
+	defer u.Done()
+
+	for _, item := range info.Items {
+		if err = o.Upload(ctx, lease, item); err != nil {
+			return nil, errors.Wrap(err, "Could not upload disks to VMWare")
+		}
+	}
+	return &info.Entity, lease.Complete(ctx)
+}
+
+// Deploy Virtual Machine to VMWare
+func Deploy(ctx context.Context, vmName string, OVAURL string, logger log.Logger, cfg *config.Config, c *vim25.Client, opts ...string) (int, error) {
+	// TODO: make up a metod to check deploy progress.
+	// Job ID and endpoint with status?
+	// keep HTTP connection with client and poll it?
+	var jid int
+
+	// -----------------------
+	// TODO: Move to separate method with signature:
+	// func (cmd *deployment) Import(pathToOVF string) (*types.ManagedObjectReference, error) {
+	// call: moref, err := d.Import(pathToOVF)
+	// start Import OVA/OVF.
+
+	// -----------------------
+	d := newDeployment(c, vmName)
+	if err := d.ChooseDatacenter(ctx, cfg.VMWare.DC); err != nil {
+		return jid, err
+	}
+
+	if err := d.ChooseDatastore(ctx, cfg.VMWare.DS); err != nil {
+		return jid, err
+	}
+
+	if err := d.ChooseResourcePool(ctx, cfg.VMWare.RP); err != nil {
+		return jid, err
+	}
+
+	if err := d.ChooseFolder(ctx, cfg.VMWare.Folder); err != nil {
+		return jid, err
+	}
+
+	if err := d.ChooseHost(ctx, cfg.VMWare.Host); err != nil {
+		return jid, err
+	}
+
+	// TODO: Download OVF. Download OVA
+	moref, err := d.Import(ctx, "vyacheslav.terdunov.test.ovf")
+	if err != nil {
+		return jid, err
+	}
+
+	vm := object.NewVirtualMachine(c, *moref)
+
+	// TODO: move to method d.Deploy
+	// PowerON
+	logger.Log("msg", "Powering on...")
+	task, err := vm.PowerOn(ctx)
+	if err != nil {
+		logger.Log("err", errors.Wrap(err, "Could not power on VM"))
+		return jid, err
+	}
+	if _, err = task.WaitForResult(ctx, nil); err != nil {
+		logger.Log("err", errors.Wrap(err, "Failed to wait powering on task"))
+	}
+
+	// WaitForIP
+	logger.Log("msg", "Waiting for ip")
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Minute)
+	defer cancel()
+
+	ip, err := vm.WaitForIP(ctx)
+	if err != nil {
+		logger.Log("err", errors.Wrap(err, "Could not get IP address"))
+		return jid, err
+	}
+	logger.Log("msg", "Received IP address", "ip", ip)
+	// end ReadOvf
+
+	logger.Log(
+		"msg", "Deploy OVA",
+		"name", vmName,
+		"ova_url", OVAURL,
+	)
+	return jid, nil
+}
+
+func newDeployment(c *vim25.Client, vmName string) *deployment {
+	finder := find.NewFinder(c, true)
+	return &deployment{
+		Client: c,
+		Finder: finder,
+		ovfx:   ovfx{Name: vmName},
+	}
+}
+
+func readOVF(fpath string) ([]byte, error) {
+	f, err := os.Open(fpath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	r, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+func readEnvelope(data []byte) (*ovf.Envelope, error) {
+	r := bytes.NewReader(data)
+
+	e, err := ovf.Unmarshal(r)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to parse ovf")
+	}
+
+	return e, nil
+}
+
 type progressLogger struct {
 	prefix string
 
@@ -215,6 +405,7 @@ func (p *progressLogger) loopB(tick *time.Ticker, ch <-chan progress.Report) err
 
 	return err
 }
+
 func (p *progressLogger) Wait() {
 	close(p.done)
 	p.wg.Wait()
@@ -224,168 +415,6 @@ func (p *progressLogger) Sink() chan<- progress.Report {
 	ch := make(chan progress.Report)
 	p.sink <- ch
 	return ch
-}
-
-// Deploy returns summary information about Virtual Machines
-func Deploy(ctx context.Context, vmName string, OVAURL string, logger log.Logger, cfg *config.Config, c *vim25.Client, opts ...string) (int, error) {
-	// TODO: make up a metod to check deploy progress.
-	// Job ID and endpoint with status?
-	// keep HTTP connection with client and poll it?
-	var jid int
-
-	// -----------------------
-	// TODO: Move to separate method with signature:
-	// func (cmd *deployment) Import(pathToOVF string) (*types.ManagedObjectReference, error) {
-	// call: moref, err := d.Import(pathToOVF)
-	// start Import OVA/OVF.
-	d := newDeployment(c, vmName)
-
-	if err := d.ChooseDatacenter(ctx, cfg.VMWare.DC); err != nil {
-		return jid, err
-	}
-
-	if err := d.ChooseDatastore(ctx, cfg.VMWare.DS); err != nil {
-		return jid, err
-	}
-
-	if err := d.ChooseResourcePool(ctx, cfg.VMWare.RP); err != nil {
-		return jid, err
-	}
-
-	if err := d.ChooseFolder(ctx, cfg.VMWare.Folder); err != nil {
-		return jid, err
-	}
-
-	if err := d.ChooseHost(ctx, cfg.VMWare.Host); err != nil {
-		return jid, err
-	}
-
-	// TODO: Download OVF. Download OVA
-	o, err := readOVF("vyacheslav.terdunov.test.ovf")
-	if err != nil {
-		logger.Log("err", err)
-		return jid, err
-	}
-
-	e, err := readEnvelope(o)
-	if err != nil {
-		logger.Log("err", err)
-		return jid, nil
-	}
-
-	name := "Virtual Appliance"
-	if e.VirtualSystem != nil {
-		name = e.VirtualSystem.ID
-		if e.VirtualSystem.Name != nil {
-			name = *e.VirtualSystem.Name
-		}
-	}
-
-	// Override name from params if specified
-	if d.Name != "" {
-		name = d.Name
-	}
-
-	cisp := types.OvfCreateImportSpecParams{
-		// See https://github.com/vmware/govmomi/blob/v0.16.0/vim25/types/enum.go#L3381-L3395
-		// VMWare can not support some of those disk format types
-		// "preallocated", "thin", "seSparse", "rdm", "rdmp",
-		// "raw", "delta", "sparse2Gb", "thick2Gb", "eagerZeroedThick",
-		// "sparseMonolithic", "flatMonolithic", "thick"
-		DiskProvisioning: "thin",
-		EntityName:       name,
-		NetworkMapping:   d.NetworkMap(e),
-	}
-
-	m := ovf.NewManager(c)
-	rp := d.ResourcePool
-	ds := d.Datastore
-	spec, err := m.CreateImportSpec(ctx, string(o), rp, ds, cisp)
-	if err != nil {
-		logger.Log("err", err)
-		return jid, err
-	}
-	if spec.Error != nil {
-		logger.Log("err", errors.New(spec.Error[0].LocalizedMessage))
-		return jid, errors.New(spec.Error[0].LocalizedMessage)
-	}
-	if spec.Warning != nil {
-		for _, w := range spec.Warning {
-			logger.Log("msg", w)
-		}
-	}
-
-	// TODO: get from params
-	anno := "Test annotations"
-	if anno != "" {
-		switch s := spec.ImportSpec.(type) {
-		case *types.VirtualMachineImportSpec:
-			s.ConfigSpec.Annotation = anno
-		case *types.VirtualAppImportSpec:
-			s.VAppConfigSpec.Annotation = anno
-		}
-	}
-
-	lease, err := rp.ImportVApp(ctx, spec.ImportSpec, d.Folder, d.Host)
-	if err != nil {
-		logger.Log("err", err)
-		return jid, err
-	}
-
-	info, err := lease.Wait(ctx, spec.FileItem)
-	if err != nil {
-		logger.Log("err", err)
-		return jid, err
-	}
-
-	u := lease.StartUpdater(ctx, info)
-	defer u.Done()
-
-	for _, item := range info.Items {
-		if err = d.Upload(ctx, lease, item); err != nil {
-			logger.Log("err", err)
-			return jid, errors.Wrap(err, "Could not upload disks to VMWare")
-		}
-	}
-
-	moref := &info.Entity
-	lease.Complete(ctx)
-	// end Import OVA/OVF
-	// -----------------------
-
-	vm := object.NewVirtualMachine(c, *moref)
-
-	// TODO: move to method d.Deploy
-	// PowerON
-	logger.Log("msg", "Powering on...")
-	task, err := vm.PowerOn(ctx)
-	if err != nil {
-		logger.Log("err", errors.Wrap(err, "Could not power on VM"))
-		return jid, err
-	}
-	if _, err = task.WaitForResult(ctx, nil); err != nil {
-		logger.Log("err", errors.Wrap(err, "Failed to wait powering on task"))
-	}
-
-	// WaitForIP
-	logger.Log("msg", "Waiting for ip")
-	ctx, cancel := context.WithTimeout(ctx, 1*time.Minute)
-	defer cancel()
-
-	ip, err := vm.WaitForIP(ctx)
-	if err != nil {
-		logger.Log("err", errors.Wrap(err, "Could not get IP address"))
-		return jid, err
-	}
-	logger.Log("msg", "Received IP address", "ip", ip)
-	// end ReadOvf
-
-	logger.Log(
-		"msg", "Deploy OVA",
-		"name", vmName,
-		"ova_url", OVAURL,
-	)
-	return jid, nil
 }
 
 func newProgressLogger(prefix string) *progressLogger {
@@ -401,39 +430,4 @@ func newProgressLogger(prefix string) *progressLogger {
 	go p.loopA()
 
 	return p
-}
-
-func newDeployment(c *vim25.Client, vmName string) *deployment {
-	finder := find.NewFinder(c, true)
-	return &deployment{
-		Client: c,
-		Finder: finder,
-		ovfx:   ovfx{Name: vmName},
-	}
-}
-
-func readOVF(fpath string) ([]byte, error) {
-	f, err := os.Open(fpath)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	r, err := ioutil.ReadAll(f)
-	if err != nil {
-		return nil, err
-	}
-
-	return r, nil
-}
-
-func readEnvelope(data []byte) (*ovf.Envelope, error) {
-	r := bytes.NewReader(data)
-
-	e, err := ovf.Unmarshal(r)
-	if err != nil {
-		return nil, errors.Wrap(err, "Failed to parse ovf")
-	}
-
-	return e, nil
 }
