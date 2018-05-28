7593b29b285eb74cf9dab1762801b4a12b91cb6d
vterdunov <mail2slick@gmail.com>
2018-05-28 17:41:12 +0300

    Fix travis release

--- build/ci/travis.sh
+++ build/ci/travis.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+DOCKER_USERNAME=$1
+COMMIT=$2
+
 # Do not rebuild/retest image that we already have.
 if [ -n "$TRAVIS_TAG" ]; then
+  echo "Found release tag"
   docker pull ${$DOCKER_USERNAME}/janna-api:${COMMIT}
   docker tag ${$DOCKER_USERNAME}/janna-api:${COMMIT} ${$DOCKER_USERNAME}/janna-api:${TRAVIS_TAG}
   exit 0
