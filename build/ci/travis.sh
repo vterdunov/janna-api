ac7b5f2aa572d9185b932363cc74315d691c94c2
vterdunov <mail2slick@gmail.com>
2018-05-28 17:45:08 +0300

    Fix typos

--- build/ci/travis.sh
+++ build/ci/travis.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DOCKER_USERNAME=$1
-COMMIT=$2
+echo $DOCKER_USERNAME
+echo $COMMIT
 
 # Do not rebuild/retest image that we already have.
 if [ -n "$TRAVIS_TAG" ]; then
   echo "Found release tag"
-  docker pull ${$DOCKER_USERNAME}/janna-api:${COMMIT}
-  docker tag ${$DOCKER_USERNAME}/janna-api:${COMMIT} ${$DOCKER_USERNAME}/janna-api:${TRAVIS_TAG}
+  docker pull ${DOCKER_USERNAME}/janna-api:${COMMIT}
+  docker tag ${DOCKER_USERNAME}/janna-api:${COMMIT} ${DOCKER_USERNAME}/janna-api:${TRAVIS_TAG}
   exit 0
 fi
 
