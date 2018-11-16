b57cfd9889e558ab0e41464d41250ace43ef7d57
vterdunov <mail2slick@gmail.com>
2018-11-16 23:38:21 +0400

    Do not lint version package

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,11 +1,10 @@
+//nolint:gochecknoglobals
 package version
 
 var (
 	// BuildTime is a time label of the moment when the binary was built. Sets at compile-time.
-	//nolint:gochecknoglobals
 	BuildTime = "unset"
 	// Commit is a last commit hash at the moment when the binary was built. Sets at compile-time.
-	//nolint:gochecknoglobals
 	Commit = "unset"
 )
 
