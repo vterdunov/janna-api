64998e248114d743601f368941e75e4db4eb2d86
Terdunov Vyacheslav <mail2slick@gmail.com>
2018-12-25 16:53:00 +0300

    Fix deploy ova (#29)
    * Do not change directory when uploading disks

--- internal/service/vm_deploy.go
+++ internal/service/vm_deploy.go
@@ -415,6 +415,7 @@ func (o *Deployment) Import(ctx context.Context, OVAURL string, anno string) (*v
 		return nil, err
 	}
 	defer os.RemoveAll(td)
+	defer o.logger.Log("msg", "Removed temp dir", "dir", td)
 
 	if untarErr := untar(td, ova); untarErr != nil {
 		o.logger.Log("err", untarErr)
@@ -506,8 +507,11 @@ func (o *Deployment) Import(ctx context.Context, OVAURL string, anno string) (*v
 	u := lease.StartUpdater(ctx, info)
 	defer u.Done()
 
-	os.Chdir(td)
 	for _, item := range info.Items {
+		// override disk path to use in cocnurent mode
+		// os.Chdir doesn't work preperly
+		item.Path = path.Join(td, item.Path)
+
 		if err = o.Upload(ctx, lease, item); err != nil {
 			return nil, errors.Wrap(err, "Could not upload disks to VMWare")
 		}
